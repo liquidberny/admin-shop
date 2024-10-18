98a6e5b9f36dcc033f6eb19d146edc742f0fc12c
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-10-18 13:18:15 -0700

    realizar proceso de autenticacion

---
+++ src/modules/auth/stores/auth.store.ts
@@ -0,0 +1,54 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+import { AuthStatus, type User } from '../interfaces';
+import { loginAction } from '../actions';
+
+export const useAuthStore = defineStore('counter', () => {
+  const authStatus = ref<AuthStatus>(AuthStatus.Checking);
+  const user = ref<User | undefined>();
+  const token = ref('');
+
+  const login = async (email: string, password: string) => {
+    try {
+      const loginResp = await loginAction(email, password);
+
+      if (!loginResp.ok) {
+        logout();
+        return false;
+      }
+
+      user.value = loginResp.user;
+      token.value = loginResp.token;
+      authStatus.value = AuthStatus.Authenticated;
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return logout();
+    }
+  };
+
+  const logout = () => {
+    authStatus.value = AuthStatus.Unauthenticated;
+    user.value = undefined;
+    token.value = '';
+    return false;
+  };
+
+  return {
+    user,
+    token,
+    authStatus,
+
+    // Getters
+    isChecking: computed(() => authStatus.value === AuthStatus.Checking),
+    isAuthenticated: computed(
+      () => authStatus.value === AuthStatus.Authenticated,
+    ),
+
+    username: computed(() => user.value?.fullName),
+
+    //Actions
+    login,
+  };
+});
--- src/modules/auth/views/LoginView.vue
+++ src/modules/auth/views/LoginView.vue
@@ -1,21 +1,23 @@
 <template>
   <h1 class="text-2xl font-semibold mb-4">Login</h1>
-  <form action="#" method="POST">
+  <form @submit.prevent="onLogin">
     <!-- Username Input -->
     <div class="mb-4">
-      <label for="username" class="block text-gray-600">Username</label>
+      <label for="username" class="block text-gray-600">Correo</label>
       <input
+        v-model="myForm.email"
         type="text"
-        id="username"
-        name="username"
+        id="email"
+        name="email"
         class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
         autocomplete="off"
       />
     </div>
     <!-- Password Input -->
     <div class="mb-4">
-      <label for="password" class="block text-gray-600">Password</label>
+      <label for="password" class="block text-gray-600">Contraseña</label>
       <input
+        v-model="myForm.password"
         type="password"
         id="password"
         name="password"
@@ -25,12 +27,18 @@
     </div>
     <!-- Remember Me Checkbox -->
     <div class="mb-4 flex items-center">
-      <input type="checkbox" id="remember" name="remember" class="text-blue-500" />
-      <label for="remember" class="text-gray-600 ml-2">Remember Me</label>
+      <input
+        v-model="myForm.rememberMe"
+        type="checkbox"
+        id="remember"
+        name="remember"
+        class="text-blue-500"
+      />
+      <label for="remember" class="text-gray-600 ml-2">Recordar usuario</label>
     </div>
     <!-- Forgot Password Link -->
     <div class="mb-6 text-blue-500">
-      <a href="#" class="hover:underline">Forgot Password?</a>
+      <a href="#" class="hover:underline">¿Olvidaste la contraseña?</a>
     </div>
     <!-- Login Button -->
     <button
@@ -43,23 +51,28 @@
   </form>
   <!-- Sign up  Link -->
   <div class="mt-6 text-blue-500 text-center">
-    <RouterLink :to="{ name: 'register' }" class="hover:underline">Sign up Here</RouterLink>
+    <RouterLink :to="{ name: 'register' }" class="hover:underline"
+      >Sign up Here</RouterLink
+    >
   </div>
 </template>
 
 <script lang="ts" setup>
+import { reactive } from 'vue';
 import { useRouter } from 'vue-router';
+import { useAuthStore } from '../stores/auth.store';
 
 const router = useRouter();
 
-const onLogin = () => {
-  localStorage.setItem('userId', 'ABC-123');
+const authStore = useAuthStore();
+const myForm = reactive({
+  email: '',
+  password: '',
+  rememberMe: false,
+});
 
-  const lastPath = localStorage.getItem('lastPath') ?? '/';
-
-  // router.replace({
-  //   // name: 'home',
-  // });
-  router.replace(lastPath);
+const onLogin = async () => {
+  const ok = await authStore.login(myForm.email, myForm.password);
+  console.log({ ok });
 };
 </script>
